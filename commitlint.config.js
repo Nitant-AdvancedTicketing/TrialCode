2fff832e34c42ad11aed72ac82c9f5256c447383
Nitant Trivedi <nitant.trivedi@oneadvanced.com>
2025-09-29 16:39:26 +0000

    fix: update hook (#12)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,18 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2, "always",
+      [
+        "release",   // custom major type
+        "feat",      // minor
+        "fix",       // patch
+        "style",     // patch
+        "refactor",  // patch
+        "chore",     // no release
+        "docs",      // no release
+        "test"       // no release
+      ]
+    ]
+  }
+};
