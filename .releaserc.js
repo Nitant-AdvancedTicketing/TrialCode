4ca3cdd62a875b78b6a6987a2ff38a7bbccb06b8
Nitant-AdvancedTicketing <nitant.trivedi@oneadvanced.com>
2025-09-29 12:35:57 +0000

    feat: update pr yaml

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,27 @@
 module.exports = {
   branches: ["master"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    ["@semantic-release/commit-analyzer", {
+      preset: "conventionalcommits",
+      releaseRules: [
+        // 🔹 Major release rules
+        { type: "release", release: "major" },      // custom type "release"
+        { breaking: true, release: "major" },       // any commit with BREAKING CHANGE or !
+        
+        // 🔹 Minor release rules
+        { type: "feat", release: "minor" },
+        
+        // 🔹 Patch release rules
+        { type: "fix", release: "patch" },
+        { type: "style", release: "patch" },
+        { type: "refactor", release: "patch" },
+        
+        // 🔹 No release rules
+        { type: "chore", release: false },
+        { type: "docs", release: false },
+        { type: "test", release: false }
+      ]
+    }],
     "@semantic-release/release-notes-generator",
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
     ["@semantic-release/git", {
